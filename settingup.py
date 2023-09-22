437dc15a4240740f0c37705a98ac4ba1a6e37ffb
Sunkwan Kim <47778502+danielkim428@users.noreply.github.com>
2023-09-22 09:52:57 -0400

    Delete settingup.py

--- settingup.py
+++
@@ -1,9 +0,0 @@
-from pet.models import *
-from django.contrib.auth.models import User
-
-user = User.objects.create_user('danyl', password='Sun042800*')
-user.is_superuser = True
-user.is_staff = True
-user.save()
-
-
